1faf3e4ab6e92cfb09335538d800322054caffba
AverySioli <snowyavery5@gmail.com>
2026-03-05 16:30:51 -0500

    create new subsystems
    Create the following subsystems:
    - Hopper
    - Indexer

--- Rebuilt 10017/src/main/include/Constants.h
+++ Rebuilt 10017/src/main/include/Constants.h
@@ -19,3 +19,16 @@ namespace OperatorConstants {
 inline constexpr int kDriverControllerPort = 0;
 
 }  // namespace OperatorConstants
+
+namespace HopperConstants {
+
+    constexpr int kHopperMotor = 0;
+    constexpr double kHopperSpeed = 0.8;
+};
+
+namespace IndexerConstants {
+
+    constexpr int kIndexerMotor = 1;
+    constexpr double kIndexerSpeed = 1;
+};
+
