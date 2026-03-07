cb9d7d37c2c13b1402d890f31d9f73ba6867eeb7
Coolakash10017 <kadelakashfirst@gmail.com>
2026-03-06 20:39:40 -0500

    Add IntakeConstants with IntakePort definition
    Added IntakeConstants namespace with IntakePort constant.

--- Rebuilt 10017/src/main/include/Constants.h
+++ Rebuilt 10017/src/main/include/Constants.h
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #pragma once
-
+#include <GlobalConstants.h>
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants.  This should not be used for any other
@@ -18,4 +18,9 @@ namespace OperatorConstants {
 
 inline constexpr int kDriverControllerPort = 0;
 
+
+
+namespace IntakeConstants{
+  constexpr int IntakePort = 1;
+};
 }  // namespace OperatorConstants
